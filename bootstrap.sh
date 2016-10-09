6cd7259805767b1219776c434c21b0ad8e23428a
Daniel Roschka <danielroschka@phoenitydawn.de>
2016-10-09 16:36:34 +0200

    Make it compatible with Ubuntu 14.04 LTS

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-WHO=`whoami`
 sudo vmdebootstrap \
-  --owner ${WHO} \
   --verbose \
   --log amd64-uefi.log \
   --log-level debug \
   --size 1G \
   --distribution jessie \
-  --debootstrapopts="variant=minbase" \
   jessie.img
